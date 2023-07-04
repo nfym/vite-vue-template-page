c8c57a420209ddaa421555699ee711bd83199637
huhaonan <huhaonan@sics.ac.cn>
2023-07-04 16:00:24 +0800

    build(env): 修改打包base路径配置为相对路径

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue action 修改 feature" />
+  <HelloWorld msg="base 设置 ./ 相对路径测试" />
 </template>
 
 <style scoped>
