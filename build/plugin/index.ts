5d8cbeedd6e043ba1b2c194cfad0271eb6bb7c3c
huhaonan <2682480394@qq.com>
2023-09-06 14:28:10 +0800

    feat: 添加@vitejs/plugin-vue-jsx

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -1,11 +1,12 @@
 import type { PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import configHtmlPlugin from './html'
 import configWebUpdateNotificationPlugin from './webUpdate'
 import configVisualizerConfig from './visualizer'
 
 export default function createVitePlugins(env: ViteEnv, isBuild: boolean) {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [vue()]
+  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()]
 
   // vite-plugin-html
   vitePlugins.push(configHtmlPlugin(env))
