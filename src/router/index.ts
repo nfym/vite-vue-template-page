025c12548c179853c7b053b0fffd3a94d8d1213c
huhaonan <2682480394@qq.com>
2023-09-08 10:42:47 +0800

    feat(router): createWebHistory base设置问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Description: 创建路由
  * @Date: 2023-09-06 16:21:03
- * @LastEditTime: 2023-09-07 16:34:36
+ * @LastEditTime: 2023-09-08 10:32:40
  */
 import { createRouter, createWebHistory } from 'vue-router'
 import { basicRoutes, modulesRoutes } from './routes'
@@ -9,7 +9,7 @@ import type { App } from 'vue'
 
 // 创建一个可以被 Vue 应用程序使用的路由实例
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH),
+  history: createWebHistory(),
   // 应该添加到路由器的初始路由列表。
   routes: [...basicRoutes, ...modulesRoutes],
   // 是否应该禁止尾部斜杠。默认 false
