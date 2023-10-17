71dd107b5d9025f7905f6dc8b7a8df7fb1332799
huhaonan <2682480394@qq.com>
2023-10-17 11:10:37 +0800

    feat(router): github使用hash路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,23 @@
  * @Date: 2023-09-06 16:21:03
  * @LastEditTime: 2023-09-18 10:08:04
  */
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory
+} from 'vue-router'
 import { basicRoutes, modulesRoutes } from './routes'
 import type { App } from 'vue'
 
+// 部署至 github 使用 hash 模式避免刷新后 404，其他平台使用 histoty
+const histoty =
+  import.meta.env.MODE === 'production'
+    ? createWebHashHistory()
+    : createWebHistory()
+
 // 创建一个可以被 Vue 应用程序使用的路由实例
 export const router = createRouter({
-  history: createWebHistory(),
+  history: histoty,
   // 应该添加到路由器的初始路由列表。
   routes: [...basicRoutes, ...modulesRoutes]
   // 是否应该禁止尾部斜杠。默认 false
